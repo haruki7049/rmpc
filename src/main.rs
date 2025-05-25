dec2d73cdc179389948153a18ac740de40c68a0c
haruki7049 <tontonkirikiri@gmail.com>
2025-05-25 18:07:04 +0900

    feat: Init clap CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use mpd::{Client, Query};
+use clap::Parser;
+use std::net::{IpAddr, Ipv4Addr};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut c = Client::connect("127.0.0.1:6600").unwrap();
+    let args: CLIArgs = CLIArgs::parse();
+
+    let mut c = Client::connect((args.ip, args.port)).unwrap();
     println!("version: {:?}", c.version);
     println!("status: {:?}", c.status());
     println!("stuff: {:?}", c.find(&Query::new(), (1, 2)));
@@ -16,4 +20,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("No song playing.");
     }
     Ok(())
+}
+
+#[derive(Debug, Parser)]
+struct CLIArgs {
+    /// An IP which references your MPD server
+    #[arg(short, long, default_value_t = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)))]
+    ip: IpAddr,
+
+    /// An IP which references your MPD server
+    #[arg(short, long, default_value_t = 6600)]
+    port: u16,
 }
